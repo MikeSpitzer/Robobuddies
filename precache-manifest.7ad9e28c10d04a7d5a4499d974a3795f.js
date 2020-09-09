199c64bdf5612eeeb0003fc2d70b16325a226af5
@Beefy <dawgofwar@gmail.com>
2020-09-09 11:27:41 -0400

    Updates

---
+++ precache-manifest.7ad9e28c10d04a7d5a4499d974a3795f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e5c902aedb34881513c4dc6c682bc4",
+    "url": "/Robobuddies/index.html"
+  },
+  {
+    "revision": "4f01921f02303c9efa15",
+    "url": "/Robobuddies/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "40ec639d33e93f339fdd",
+    "url": "/Robobuddies/static/css/main.55a3fc23.chunk.css"
+  },
+  {
+    "revision": "4f01921f02303c9efa15",
+    "url": "/Robobuddies/static/js/2.e098fac4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robobuddies/static/js/2.e098fac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ec639d33e93f339fdd",
+    "url": "/Robobuddies/static/js/main.ab90ca94.chunk.js"
+  },
+  {
+    "revision": "84fda067d2714a632a34",
+    "url": "/Robobuddies/static/js/runtime-main.78fd9c8d.js"
+  },
+  {
+    "revision": "f0fff6835728cb1124cf2ec7cbfc6105",
+    "url": "/Robobuddies/static/media/AtariClassic-gry3.f0fff683.woff"
+  },
+  {
+    "revision": "0e2245d8a4cdc467fc5238e7ea5cfc57",
+    "url": "/Robobuddies/static/media/AtariClassicChunky-PxXP.0e2245d8.woff"
+  },
+  {
+    "revision": "a8b3207f41307e5b523746dbe193c87f",
+    "url": "/Robobuddies/static/media/AtariClassicExtrasmooth-LxZy.a8b3207f.woff"
+  },
+  {
+    "revision": "8c8978dbb2a015bacebbf554a597000b",
+    "url": "/Robobuddies/static/media/AtariClassicSmooth-XzW2.8c8978db.woff"
+  }
+]);
